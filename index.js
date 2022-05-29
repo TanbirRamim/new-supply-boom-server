f78230e27edc73f0655832263f59b453bdea3443
TanbirRamim <Tanbirramim420@gmail.com>
2022-05-29 23:15:19 +0600

    made mistake

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app = express();
-const port = process.env.PORT || 5000;
+// const express = require("express");
+// const cors = require("cors");
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const app = express();
+// const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
@@ -77,14 +77,14 @@ async function run() {
       res.send(result);
     });
 
-    // app.get("/orders", async (req, res) => {
-    //   const email = req.query.email;
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
 
-    //   const query = { email: email };
-    //   const cursor = orderCollection.find(query);
-    //   const orders = await cursor.toArray();
-    //   res.send(orders);
-    // });
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
     app.post("/orders", async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
